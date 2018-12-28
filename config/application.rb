19a06275409bf99f682e0b47e41fc55df6769b56
Christopher Pham <chrismpham@gmail.com>
2018-12-28 10:05:00 +0200

    Add put, patch, delete to CORS

--- config/application.rb
+++ config/application.rb
@@ -42,7 +42,7 @@ module GoAppApi
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins ENV.fetch('ALLOWED_CORS_ORIGINS')
-        resource '*', headers: :any, methods: %i[get post options]
+        resource '*', headers: :any, methods: %i[get post put patch delete options]
       end
     end
   end
