9e25bdf46af32638e6498d709caae99ce88a0873
Christopher Pham <chrismpham@gmail.com>
2019-01-09 12:58:37 +0200

    [APP-151] Fix-1

--- app/serializers/companies/overview_serializer.rb
+++ app/serializers/companies/overview_serializer.rb
@@ -1,6 +1,6 @@
 module Companies
   class OverviewSerializer < ApiSerializer
-    attributes :title, :description, :phone_number, :email, :facebook_profile_link, :linkedin_profile_link, 
+    attributes :id, :title, :description, :phone_number, :email, :facebook_profile_link, :linkedin_profile_link, 
                :instagram_profile_link, :snapchat_profile_link, :website_link, :location
   end
 end
