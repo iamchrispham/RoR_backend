6acc3792a46896fe82822b613293cd097166c1b9
Christopher Pham <chrismpham@gmail.com>
2019-02-14 10:37:45 +0200

    [#APP-470] add follow/unfollow for companies

--- app/controllers/api/v1/users/followed_companies_controller.rb
+++ app/controllers/api/v1/users/followed_companies_controller.rb
@@ -2,11 +2,33 @@ module Api
   module V1
     module Users
       class FollowedCompaniesController < ApiController
+        before_action :set_company, only: [:create, :destroy]
+
         def index
           success_response(
             count: current_api_user.followed_companies.count,
             companies: current_api_user.followed_companies.ordered_by_name.limit(limit).offset(offset).map { |followed_companies| followed_companies.cached(current_api_user, type: :feed) }
           )
+        end
+
+        def create
+          followed = current_api_user.users_followed_companies.new(company_id: @company.id)
+          if followed.save
+            success_response()
+          else
+            active_record_error_response(followed)
+          end
+        end
+
+        def destroy
+          current_api_user.followed_companies.delete(@company)
+          success_response()
+        end
+
+        private
+
+        def set_company
+          @company ||= Company.find_by(id: params[:company_id] || params[:id])
         end
       end
     end
--- app/models/users_followed_company.rb
+++ app/models/users_followed_company.rb
@@ -1,4 +1,6 @@
 class UsersFollowedCompany < ActiveRecord::Base
+  validates :company_id, uniqueness: { scope: :user_id }
+
   belongs_to :user
   belongs_to :company
 end
